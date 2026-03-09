d5f842d201b24abad24e447b5673e2783744e36a
mariem457 <mariemhamdi021@gmail.com>
2026-03-09 14:45:53 +0100

    fix folders - add full backend and frontend

---
+++ frontend/src/app/features/admin-principal/pages/admin-principal-dashboard/admin-principal-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-principal-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-principal-dashboard.component.html',
+  styleUrl: './admin-principal-dashboard.component.css'
+})
+export class AdminPrincipalDashboardComponent {
+
+}
